6101608ce9044895eb6eae84c50cf14b0c80b044
Karol Krzak <80320206+BigQszak@users.noreply.github.com>
2023-09-27 00:11:15 +0200

    Update loss.py
    Finished implementing loss functions for specific outputs of YOLO model

--- src/loss.py
+++ src/loss.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 
-
 from utils import intersection_over_union as iou
 
 class YOLOLoss(nn.Module):
@@ -28,11 +27,27 @@ class YOLOLoss(nn.Module):
         )
 
         # Object loss
-        
+        anchors = anchors.reshape(1, 3, 1, 1, 2) 
+        box_predictions = torch.cat([self.sigmoid(predictions[...,1:3]), torch.exp(predictions[...,3:5] * anchors)], dim=-1)
+        ious = iou(box_predictions[obj], target[...,1:5][obj]).detach()
+        object_loss = self.mse((predictions[..., 0:1][obj]), (ious * target[..., 0:1]))
 
         # Box coordinate loss
+        predictions[..., 1:3] = self.sigmoid(predictions[..., 1:3]) # x & y between 0 and 1
+        target[..., 3:5] = torch.log(
+            (1e-16 +  target[..., 3:5] / anchors) 
+        )
+        box_loss = self.mse(predictions[..., 1:5][obj], target[..., 1:5][obj])    
 
         # Class loss
+        class_loss = self.entropy(
+            (predictions[...,5:][obj]), (target[..., 5:][obj].long())
+        )
 
-
+        return (
+            self.lambda_box * box_loss 
+            + self.lambda_obj * object_loss 
+            + self.lambda_no_obj * no_obj_loss 
+            + self.lambda_class * class_loss
+        )
 
